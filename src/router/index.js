9f8033118998128b2ff5d4534d3bf41899319f9d
ChenYue <3309168033@qq.com>
2023-05-02 21:18:46 +0800

    update--修改路由模式为history update--添加默认路由前缀

--- src/router/index.js
+++ src/router/index.js
@@ -7,21 +7,23 @@ import Field from '@/components/Field'
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Database',
-      component: Database
-    },
-    {
-      path: '/table',
-      name: 'Table',
-      component: Table
-    },
-    {
-      path: '/field',
-      name: 'Field',
-      component: Field
-    }
-  ]
+    routes: [
+        {
+            path: '/',
+            name: 'Database',
+            component: Database
+        },
+        {
+            path: '/table',
+            name: 'Table',
+            component: Table
+        },
+        {
+            path: '/field',
+            name: 'Field',
+            component: Field
+        }
+    ],
+    mode: "history",
+    base: "/"
 })
